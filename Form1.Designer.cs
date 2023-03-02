ed18195a8dfde69b80469624facaf7a90fb0a8fd
Aldratov <arni2010@mail.ru>
2023-03-02 15:53:59 +0300

    first

---
+++ Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿
+namespace Counter_v1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.bt_count = new System.Windows.Forms.Button();
+            this.tB_in = new System.Windows.Forms.TextBox();
+            this.tB_out = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.cB_report = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.bt_folder = new System.Windows.Forms.Button();
+            this.tB_path = new System.Windows.Forms.TextBox();
+            this.lb_path = new System.Windows.Forms.Label();
+            this.bt_load = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tB_currentPath = new System.Windows.Forms.TextBox();
+            this.bt_print = new System.Windows.Forms.Button();
+            this.bt_plan = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bt_count
+            // 
+            this.bt_count.Location = new System.Drawing.Point(12, 702);
+            this.bt_count.Name = "bt_count";
+            this.bt_count.Size = new System.Drawing.Size(65, 23);
+            this.bt_count.TabIndex = 0;
+            this.bt_count.Text = "Count";
+            this.bt_count.UseVisualStyleBackColor = true;
+            this.bt_count.Click += new System.EventHandler(this.bt_count_Click);
+            // 
+            // tB_in
+            // 
+            this.tB_in.Location = new System.Drawing.Point(51, 23);
+            this.tB_in.Name = "tB_in";
+            this.tB_in.Size = new System.Drawing.Size(255, 20);
+            this.tB_in.TabIndex = 1;
+            // 
+            // tB_out
+            // 
+            this.tB_out.Location = new System.Drawing.Point(12, 117);
+            this.tB_out.Multiline = true;
+            this.tB_out.Name = "tB_out";
+            this.tB_out.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tB_out.Size = new System.Drawing.Size(294, 578);
+            this.tB_out.TabIndex = 2;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(51, 3);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(139, 20);
+            this.dateTimePicker1.TabIndex = 3;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // cB_report
+            // 
+            this.cB_report.AutoSize = true;
+            this.cB_report.Location = new System.Drawing.Point(228, 5);
+            this.cB_report.Name = "cB_report";
+            this.cB_report.Size = new System.Drawing.Size(58, 17);
+            this.cB_report.TabIndex = 4;
+            this.cB_report.Text = "Report";
+            this.cB_report.UseVisualStyleBackColor = true;
+            this.cB_report.CheckedChanged += new System.EventHandler(this.cB_report_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Date:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "PCB:";
+            // 
+            // bt_folder
+            // 
+            this.bt_folder.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bt_folder.BackgroundImage")));
+            this.bt_folder.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.bt_folder.Cursor = System.Windows.Forms.Cursors.Default;
+            this.bt_folder.Location = new System.Drawing.Point(197, 3);
+            this.bt_folder.Name = "bt_folder";
+            this.bt_folder.Size = new System.Drawing.Size(25, 19);
+            this.bt_folder.TabIndex = 6;
+            this.bt_folder.UseVisualStyleBackColor = true;
+            this.bt_folder.Click += new System.EventHandler(this.bt_folder_Click);
+            // 
+            // tB_path
+            // 
+            this.tB_path.Enabled = false;
+            this.tB_path.Location = new System.Drawing.Point(83, 49);
+            this.tB_path.Name = "tB_path";
+            this.tB_path.Size = new System.Drawing.Size(223, 20);
+            this.tB_path.TabIndex = 1;
+            // 
+            // lb_path
+            // 
+            this.lb_path.AutoSize = true;
+            this.lb_path.Location = new System.Drawing.Point(12, 82);
+            this.lb_path.Name = "lb_path";
+            this.lb_path.Size = new System.Drawing.Size(51, 13);
+            this.lb_path.TabIndex = 5;
+            this.lb_path.Text = "File Path:";
+            // 
+            // bt_load
+            // 
+            this.bt_load.Location = new System.Drawing.Point(83, 701);
+            this.bt_load.Name = "bt_load";
+            this.bt_load.Size = new System.Drawing.Size(81, 25);
+            this.bt_load.TabIndex = 7;
+            this.bt_load.Text = "Load_data";
+            this.bt_load.UseVisualStyleBackColor = true;
+            this.bt_load.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Report Path:";
+            // 
+            // tB_currentPath
+            // 
+            this.tB_currentPath.Enabled = false;
+            this.tB_currentPath.Location = new System.Drawing.Point(83, 75);
+            this.tB_currentPath.Name = "tB_currentPath";
+            this.tB_currentPath.Size = new System.Drawing.Size(223, 20);
+            this.tB_currentPath.TabIndex = 1;
+            // 
+            // bt_print
+            // 
+            this.bt_print.Location = new System.Drawing.Point(170, 702);
+            this.bt_print.Name = "bt_print";
+            this.bt_print.Size = new System.Drawing.Size(64, 23);
+            this.bt_print.TabIndex = 8;
+            this.bt_print.Text = "Print";
+            this.bt_print.UseVisualStyleBackColor = true;
+            this.bt_print.Click += new System.EventHandler(this.bt_print_Click);
+            // 
+            // bt_plan
+            // 
+            this.bt_plan.Location = new System.Drawing.Point(242, 703);
+            this.bt_plan.Name = "bt_plan";
+            this.bt_plan.Size = new System.Drawing.Size(64, 23);
+            this.bt_plan.TabIndex = 8;
+            this.bt_plan.Text = "Plan";
+            this.bt_plan.UseVisualStyleBackColor = true;
+            this.bt_plan.Click += new System.EventHandler(this.bt_print_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(317, 737);
+            this.Controls.Add(this.bt_plan);
+            this.Controls.Add(this.bt_print);
+            this.Controls.Add(this.bt_load);
+            this.Controls.Add(this.bt_folder);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lb_path);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cB_report);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.tB_currentPath);
+            this.Controls.Add(this.tB_path);
+            this.Controls.Add(this.tB_out);
+            this.Controls.Add(this.tB_in);
+            this.Controls.Add(this.bt_count);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Counter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt_count;
+        private System.Windows.Forms.TextBox tB_in;
+        private System.Windows.Forms.TextBox tB_out;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.CheckBox cB_report;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button bt_folder;
+        private System.Windows.Forms.TextBox tB_path;
+        private System.Windows.Forms.Label lb_path;
+        private System.Windows.Forms.Button bt_load;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tB_currentPath;
+        private System.Windows.Forms.Button bt_print;
+        private System.Windows.Forms.Button bt_plan;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,413 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Counter_v1
+{
+    public partial class Form1 : Form
+    {
+        DateTime dt1,
+                 dt2;
+
+
+        // string dir1 = "C:\\placeALLSetup2016\\INT";
+
+        string dir1 = @"D:\8_temp\123";
+        string reportsPath = @"D:\8_temp\123\";
+        string fitschDir = @"\\placeall-pc\INT";
+
+
+
+        //строки определяющие начачало и конец
+        string startprog = "StartPlc";
+        string stopprog = "PlcComplete";
+        //string progName = "";
+        Dictionary<string, string> pName = new Dictionary<string,string>();
+        
+
+
+        //класс для работы с одним запуском
+        public class Work
+        {
+            public DateTime start { get; set; }
+            public DateTime end { get; set; }
+            public TimeSpan duration { get; set; }
+            public TimeSpan stop { get; set; }
+            public string logName{ get; set; }
+
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            //формат выбора дат
+            dateTimePicker1.Format = DateTimePickerFormat.Short;
+
+        }
+
+        //выбор папки
+        private void bt_folder_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    dir1 = fbd.SelectedPath;
+                    reportsPath = fbd.SelectedPath + "\\";
+                    tB_currentPath.Text = fbd.SelectedPath;
+                    tB_path.Text = reportsPath;
+                }
+            }
+        }
+
+
+
+        //обработка кнопки count
+        private void bt_count_Click(object sender, EventArgs e)
+        {
+            countwork();
+
+        }
+
+        //изменение даты
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            countwork();
+        }
+
+
+        // при загрузке формы загрузить путь
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tB_currentPath.Text = dir1;
+            tB_path.Text = reportsPath;
+        }
+
+
+        //загрузка данных
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Cursor = Cursors.WaitCursor; //установить курсор ожидания
+            try
+            {
+
+                DirectoryInfo dirInfo = new DirectoryInfo(fitschDir);
+                foreach (FileInfo file in dirInfo.GetFiles().Where(d=>d.LastWriteTime.Date == dt1 && d.Name.Contains("prod")))
+                {
+                    File.Copy(file.FullName, dir1 + "\\" + file.Name, true);
+                }
+                var file2 = dirInfo.GetFiles().Where(d =>d.Name.Contains("system.log")).First();
+                File.Copy(file2.FullName, dir1 + "\\" + file2.Name, true);
+                MessageBox.Show("Файлы загружены");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            Cursor = Cursors.Default;//вернуть курсор по умолчанию
+
+        }
+
+        private void cB_report_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        //функция посчёта
+        private void countwork()
+        {
+           
+            //очистить словарь
+            pName.Clear();
+            
+            
+            //clear textbox
+            tB_out.Text = "";
+
+
+            //get date
+            dt1 = dateTimePicker1.Value.Date;// получаем только дату
+            dt2 = dt1.AddDays(1);
+
+            string fullpath = "";
+
+
+            //Директория с файлами логов
+            var mydir = new DirectoryInfo(dir1);
+            // FileInfo[] files = mydir.GetFiles().Where(file => file.LastWriteTime >= dt1 && file.LastWriteTime <= dt2).OrderBy(p => p.LastWriteTime).ToArray(); //разница между датами  отбор по дате изменения
+            //CreationTime  дата создания
+
+
+
+            //Получаем список всех файлов в массив 
+            FileInfo[] files2 = mydir.GetFiles().Where(p => p.LastWriteTime.Date == dt1 && p.Name.Contains("prod")).OrderBy(p => p.LastWriteTime).ToArray();
+            //.Where(file => file.LastWriteTime >= dt1 && file.LastWriteTime <= dt2);
+
+
+            // Work[] work = new Work[files2.Length];  //массив
+
+            //Создаём список запуков
+            List<Work> works = new List<Work>();
+
+            int i = 0;
+            int j = 1;//счётчик запусков
+            TimeSpan allDuration = new TimeSpan();//общее время запусков
+            TimeSpan allStop = new TimeSpan(); //общее время простоя
+            DateTime laststop = new DateTime(); // остановка предыдущей программы
+            TimeSpan maxStopDuration= new TimeSpan();
+            maxStopDuration = TimeSpan.FromMinutes(3);
+
+
+            //Получить имя программы
+            printWork();
+
+            //перебор файлов и записываем параметры запусков
+            foreach (FileInfo file in files2)
+            {
+                // work[i] = new Work();
+
+                //Создаём экземпляк класса для оценки времени
+                Work w1 = new Work();
+
+                string[] lines = File.ReadAllLines(file.FullName);
+
+                foreach (string s in lines)
+                {
+
+                    //если строка содержит startprog
+                    if (s.Contains(startprog))
+                    {
+                        w1.start = DateTime.Parse(s.Substring(0, 19));
+                        //work[i].start = DateTime.Parse(s.Substring(0, 19));
+                    }
+
+
+                    //если строка содержит stopprog
+                    if (s.Contains(stopprog))
+                    {
+                        w1.end = DateTime.Parse(s.Substring(0, 19));
+                        // work[i].stop = DateTime.Parse(s.Substring(0, 19));
+                    }
+                                      
+                }
+
+                if (w1.end == DateTime.MinValue) //Minvale как null  у DateTime
+                {
+                    w1.end = DateTime.Parse(lines[lines.Length - 1].Substring(0,19));
+                }
+
+                if ((w1.start.Date == dt1.Date) && (w1.end.Date == dt1.Date))
+                {
+                    w1.duration = w1.end.Subtract(w1.start);
+                    w1.logName = file.Name;
+                    works.Add(w1);
+                }
+                i++;
+
+            }
+
+
+            //если нужен файл отчёта
+            if (cB_report.Checked)
+            {
+                //создаём файл отчёта
+                string filename = "Report_" + dateTimePicker1.Value.ToString("ddd_dd_MMM_yyyy");
+                fullpath = reportsPath + filename;
+
+                //если файл не сущестувует
+                if (!File.Exists(fullpath + ".txt"))
+                {
+                    File.Create(fullpath + ".txt").Dispose();
+                    fullpath += ".txt";
+                }
+                else //создаём с новым именем добавляя число
+                {
+                    int i_2 = 1;
+                    bool newname = false;
+
+                    while (!newname)
+                    {
+                        string fullpath1 = fullpath + "_" + i_2.ToString() + ".txt";
+                        if (!File.Exists(fullpath1))
+                        {
+                            File.Create(fullpath1).Dispose();
+                            fullpath = fullpath1;
+                            newname = true;
+                            break;
+                        }
+                        i_2++;
+                        if (i_2 > 100) break;
+                    }
+                }
+            }
+
+
+            //выводим содержимое в Textbox
+            StringBuilder sb = new StringBuilder();
+            foreach (Work w in works)
+            {
+
+                if (j != 1)
+                {
+                   
+                    w.stop = w.start.Subtract(laststop);
+                    string s = w.stop > maxStopDuration ? "     ###" : "";
+                    sb.Append("------------------------------------------------------------------------------------------\r\n");
+                    sb.Append(" Простой" + "\t" + w.stop.ToString() + s +"\r\n");
+                    sb.Append("------------------------------------------------------------------------------------------\r\n");
+                    allStop += w.stop;
+                }
+
+                sb.Append("\t" + "Запуск №" + j.ToString() + "\r\n" + "\r\n");
+               // sb.Append(" Файл" + "\t" + "\t" + w.logName + "\r\n" + "\r\n");
+
+                sb.Append(" Старт" + "\t" + "\t" + w.start.ToLongTimeString() + "\r\n");
+                sb.Append(" Остановка" + "\t" + w.end.ToLongTimeString() + "\r\n" + "\r\n");
+
+                //выводим время работы и добавляем стрелку html код для unikod с помощью char
+                sb.Append(" Время работы" + "\t" + w.duration.ToString() + " " + char.ConvertFromUtf32(8592) + "\r\n" + "\r\n");
+
+
+                laststop = w.end;
+                allDuration += w.duration;
+                j++;
+
+            }
+
+            //больше среднего запусков
+
+            double everWork = allDuration.TotalMilliseconds / j;//среднее время запуска
+            int aboveEverwor=0;// количество выше среднего
+            foreach (Work w in works)
+            {
+                if (w.duration.TotalMilliseconds>everWork)
+                {
+                    aboveEverwor++;
+                }
+            }
+
+
+                if (j - 1 == 0)
+            {
+                sb.Append("=============================================" + "\r\n");
+                sb.Append("\t" + "     НЕТ ДАННЫХ НА ЭТУ ДАТУ" + "\r\n");
+                sb.Append("=============================================" + "\r\n");
+            }
+            else
+            {
+                TimeSpan res;
+                res = TimeSpan.FromMilliseconds(allStop.TotalMilliseconds / j );
+                sb.Append("=============================================" + "\r\n");
+                //tB_out.Text += " Плата " + tB_in.Text + "\r\n";
+                sb.Append(" Количество запусков:" + "\t" + j.ToString() + "\r\n");
+                TimeSpan everTime = TimeSpan.FromMilliseconds(everWork);
+                sb.Append(" Запусков > "+ everTime.ToString(@"mm\:ss") +" среднего " +"\t" + aboveEverwor.ToString() + "\r\n");
+                sb.Append(" Время работы всей:" + "\t" + allDuration.ToString() + "\r\n");
+                sb.Append(" Общее время простоя:" + "\t" + allStop.ToString() + "\r\n" + "\r\n");
+                sb.Append(" Среднее время простоя:" + "\t" + res.ToString(@"mm\:ss") + "\r\n" + "\r\n"); // формат вывода timespan                
+
+
+                if (pName.Count > 0)
+                {
+                    foreach (var progr in pName)
+                    {
+                        sb.Append("Время загрузки: " + progr.Key + "\r\n");
+                        sb.Append("Имя программы: " + progr.Value + "\r\n" + "\r\n");
+                    }
+                }else
+                {
+                    sb.Append("Файл system.log не найден!"+"\r\n");
+                }
+
+
+            }
+            tB_out.Text=sb.ToString();
+
+
+            //прокрутка вниз
+            tB_out.SelectionStart = tB_out.Text.Length;
+            tB_out.ScrollToCaret();
+
+
+            //tB_out.Text = dt2.ToString();
+
+
+            //Записываем в файл
+            if (cB_report.Checked)
+            {
+                using (TextWriter tw = new StreamWriter(fullpath))
+                {
+                    tw.Write(tB_out.Text);
+                }
+            }
+        }
+        //вывод на печать
+        private void bt_print_Click(object sender, EventArgs e)
+        {
+            // объект для печати
+            PrintDocument pD = new PrintDocument();
+            // обработчик события печати
+            pD.PrintPage += PrintPageHandler;
+            pD.PrinterSettings.PrintRange = PrintRange.AllPages;
+            
+            // диалог настройки печати
+            PrintDialog printDialog = new PrintDialog();
+
+            // установка объекта печати для его настройки
+            printDialog.Document = pD;
+
+
+            // если в диалоге было нажато ОК
+            if (printDialog.ShowDialog() == DialogResult.OK)
+                printDialog.Document.Print(); // печатаем
+
+        }
+
+        private void PrintPageHandler(object sender, PrintPageEventArgs e)
+        {
+            // печать строки result
+            e.Graphics.DrawString(tB_out.Text, new Font("Arial", 6), Brushes.Black, 0, 0);
+        }
+
+        //получить имя программ
+
+        private void printWork()
+        {
+            
+            string logpath = reportsPath + "system.log";
+            if (File.Exists(logpath))
+            {
+                string[] lines = File.ReadAllLines(logpath);
+                string progTime;
+                string progName;
+
+                foreach (string s in lines)
+                {
+                    if (s.Contains(dateTimePicker1.Value.ToString("yyyy-MM-dd")))
+                    {
+                        if (s.Contains("Load File"))
+                        {
+                            progName = s.Substring(s.LastIndexOf("\\") + 1, (s.IndexOf(".spp") - s.LastIndexOf("\\")));
+                            progTime = s.Substring(11, 8);
+                            pName.Add(progTime, progName);
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
